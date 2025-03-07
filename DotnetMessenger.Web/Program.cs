71496c0784c9579e6c9039afe538efda039f8147
kyoko <crashed9001@gmail.com>
2025-03-07 19:41:29 +0300

    added otel infrastructure

--- DotnetMessenger.Web/Program.cs
+++ DotnetMessenger.Web/Program.cs
@@ -9,8 +9,8 @@ Log.Logger = new LoggerConfiguration()
     {  
         const string serviceName = "dotnet.messenger.web";
         
-        x.Endpoint = "http://localhost:5341/ingest/otlp/v1/logs";
-        // x.Endpoint = "http://localhost:3100/otlp";
+        // x.Endpoint = "http://localhost:5341/ingest/otlp/v1/logs";
+        x.Endpoint = "http://localhost:4318";
         x.Protocol = OtlpProtocol.HttpProtobuf;
         x.Headers = new Dictionary<string, string>()
         {
